02d290101382301697dd4bdd86f9db4a85719c1e
awais512 <raza.awais25@gmail.com>
2020-12-31 12:02:27 +0500

    Footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -19,7 +19,7 @@ const Layout = ({ children }) => {
       <Navbar toggleSidebar={toggleSidebar} />
       <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar} />
       <main>{children}</main>
-      {/* <Footer /> */}
+      <Footer />
     </>
   )
 }
