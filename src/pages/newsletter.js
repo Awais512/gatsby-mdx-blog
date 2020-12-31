f8d4cfda8cf78887143433b9153952dc09780b23
awais512 <raza.awais25@gmail.com>
2020-12-31 12:18:50 +0500

    Newsletter  Page

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -1,7 +1,33 @@
 import React from 'react'
 import Layout from '../components/Layout'
 const NewsLetter = () => {
-  return <h2>newsletter page</h2>
+  return (
+    <Layout>
+      <section className="newsletter-page">
+        <div className="page-center">
+          <h2>Get all the latest stories to your inbox</h2>
+          <h4>I write to my friends every weekend</h4>
+          <form className="contact-form">
+            <input
+              type="text"
+              name="name"
+              placeholder="Your name"
+              className="form-control"
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Your email"
+              className="form-control"
+            />
+            <button className="btn form-control submit-btn" type="submit">
+              Subscribe
+            </button>
+          </form>
+        </div>
+      </section>
+    </Layout>
+  )
 }
 
 export default NewsLetter
